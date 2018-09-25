05b005eb98698a70b01cfb1f3ca37d2a1b1aa906
Garry Dixon <garry.dixon@uk.renaultsportracing.com>
2018-09-25 18:01:04 +0100

    Add pagination to the searchPackages function

--- public/search.php
+++ public/search.php
@@ -2,12 +2,13 @@
 require(__DIR__ . '/../inc/core.php');
 require(__DIR__ . '/../inc/feedwriter.php');
 
-// TODO: Pagination
 $results = DB::searchPackages([
 	'includePrerelease' => !empty($_GET['includeprerelease']),
 	'orderBy' => isset($_GET['$orderby']) ? $_GET['$orderby'] : '',
 	'filter' => isset($_GET['$filter']) ? $_GET['$filter'] : '',
 	'searchQuery' => isset($_GET['searchterm']) ? trim($_GET['searchterm'], '\'') : '',
+	'top' => isset($_GET['$top']) ? $_GET['$top'] : '',
+	'offset' => isset($_GET['$skip']) ? $_GET['$skip'] : ''
 ]);
 $feed = new FeedWriter('Search');
 $feed->writeToOutput($results);
